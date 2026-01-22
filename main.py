d85f194c0d469cc18266f3dd082c3664b848a6aa
AMN-47 <akhilnagar57@gmail.com>
2026-01-22 19:16:24 +0200

    added all coding into a function

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-shift = 5 
-shifted_alphabet = (alphabet[shift:]) + alphabet[:shift] 
+def caesar():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    shift = 5 
+    shifted_alphabet = (alphabet[shift:]) + alphabet[:shift] 
+    translation_table = str.maketrans(alphabet, shifted_alphabet)
+    text = "hello world"
+    encrypted_text = (text.translate(translation_table)) 
+    print(encrypted_text)
 
-translation_table = str.maketrans(alphabet, shifted_alphabet)
-text = "hello world"
-
-encrypted_text = (text.translate(translation_table)) 
-
-print(encrypted_text)
