06de09374969644c75eda3716ff15e53a224928e
AMN-47 <akhilnagar57@gmail.com>
2026-01-22 19:09:19 +0200

    used maketrans to link strings instead of manually printing the cipher

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 shift = 5 
-shifted_alphabet = (alphabet[shift:]) + alphabet[:shift]
+shifted_alphabet = (alphabet[shift:]) + alphabet[:shift] 
 
-print(shifted_alphabet)
+translation_table = str.maketrans(alphabet, shifted_alphabet)
+
