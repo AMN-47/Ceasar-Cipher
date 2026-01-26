c158ab72f93f0aea14dcef4cf604d99cdd6fcb81
AMN-47 <akhilnagar57@gmail.com>
2026-01-26 19:09:42 +0200

    Added isinstance argument to ensure that shift value handles errors if input is not an integer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def caesar(text, shift):
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     shifted_alphabet = (alphabet[shift:]) + alphabet[:shift] 
     translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
